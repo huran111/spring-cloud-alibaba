ea99b6d8148bb4ee59bae8f5f17eb24f74a3beca
BUCHU <710625937@qq.com>
2020-03-05 11:22:38 +0800

    spring cloud 2020

---
+++ cloud-provider-payment/src/main/java/com/hr/PayMentApplication.java
@@ -0,0 +1,11 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayMentApplication.class, args);
+    }
+}
