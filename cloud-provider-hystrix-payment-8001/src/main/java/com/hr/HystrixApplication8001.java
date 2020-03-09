743e0122fb2fa59abaeea62bc564e05defa92cd3
huran111 <134522890@qq.com>
2020-03-09 20:49:15 +0800

    gateway

--- cloud-provider-hystrix-payment-8001/src/main/java/com/hr/HystrixApplication8001.java
+++ cloud-provider-hystrix-payment-8001/src/main/java/com/hr/HystrixApplication8001.java
@@ -1,14 +1,28 @@
 package com.hr;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-@EnableCircuitBreaker
+import org.springframework.context.annotation.Bean;
+
+@EnableCircuitBreaker//熔断
 @EnableEurekaClient
 @SpringBootApplication
 public class HystrixApplication8001 {
     public static void main(String[] args) {
         SpringApplication.run(HystrixApplication8001.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServket() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
